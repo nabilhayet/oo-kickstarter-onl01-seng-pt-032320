c4610d02471eed0dcf35110faba062c61d5b3faf
Nabil Hayet <nabil.bracu@gmail.com>
2020-04-23 03:02:00 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,16 @@
+class Backer 
+  attr_reader :backed_projects
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name 
+    @backed_projects=[]
+  end 
+  
+  def back_project(project)
+    @backed_projects << project 
+    if project.backers.include?(self)==false 
+      project.add_backer(self)
+    end 
+  end 
+end 
---
+++ lib/project.rb
@@ -0,0 +1,17 @@
+class Project 
+  attr_reader :backers
+  attr_accessor :title
+  
+  def initialize(title)
+    @title = title
+    @backers=[]
+  end 
+  
+  def add_backer(backer)
+    @backers << backer 
+    if backer.backed_projects.include?(self)==false
+      backer.back_project(self)
+    end 
+  end 
+
+end 
