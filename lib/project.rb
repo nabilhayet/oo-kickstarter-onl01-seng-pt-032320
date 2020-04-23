2c704bd48b64d7ca26da5018c0b8cf6d1f227351
Nabil Hayet <nabil.bracu@gmail.com>
2020-04-23 02:54:57 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,16 @@
+class Project 
+  attr_reader :backers
+  attr_accessor :title
+  
+  def initialize(title)
+    @title = title
+    @backers=[]
+  end 
+  
+  def add_backer(backer)
+    @backers << backer 
+    backer.back_project(self)
+    
+  end 
+
+end 
